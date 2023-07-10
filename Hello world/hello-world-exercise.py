545faf586fe40d78476235d5b5a3cd5f659adbe6
Vali <56320456+s-valentin@users.noreply.github.com>
2023-07-11 00:32:06 +0300

    adaugat exercitii hello-world

---
+++ Hello world/hello-world-exercise.py
@@ -0,0 +1,33 @@
+# 1. Creaza 5 variabile de tipuri diferite (doua numere intregi, rational, sir de caractere,
+# boolean). Afiseaza toate aceste variabile.
+#
+# 2. Afiseaza rezultatul a 3 operatii cu numerele intregi.
+#
+# 3. Arata care este numarul mai mare dintre cele 2 numere intregi folosind 'if'.
+#
+# 4. Daca variabila booleana este adevarata, concateneaza la sirul de caractere sirul
+# 'adev vtm', altfel 'nu se poate asa ceva'.
+#
+# 5. Creaza o lista de numere intregi neordonate (cel putin 5 numere). Ordoneaza descrescator
+# lista, apoi elimina al 3-lea element din lista.
+#
+# 6. Adauga la finalul listei un numar.
+# BONUS: Adauga inca un numar pe pozitia a 2-a
+
+# Notes:
+# - La fiecare exercitiu afiseaza rezultatul folosind print()
+# - Poti face exercitiile in fisierul asta sau poti crea unul numit 'solution.py'
+# - Grija sa pui noul fisier in folderul 'Hello world'
+# - La cuvintele in limba romana cel mai probabil vor fi subliniate fiindca PyCharm nu le cunoaste
+# - Variabilele incearca sa le denumesti cat mai sugestiv poti
+# - Intre exercitii fa un print care sa arate asa print(----- Ex [numar] -----)
+# - Intre exericitii in cod pune un comentariu asemanator cu print-ul
+# - Daca tii apasat CTRL(CMD) si apesi pe un link, te redirectioneaza catre site
+
+# Resurse:
+# https://www.w3schools.com/python/python_lists.asp
+# https://www.w3schools.com/python/python_booleans.asp
+# https://www.w3schools.com/python/python_operators.asp
+# https://www.w3schools.com/python/python_strings_concatenate.asp
+# https://www.w3schools.com/python/python_lists_methods.asp
+# https://www.w3schools.com/python/python_conditions.asp
